ec72a1e013e24afbb93042c07c36987591769786
Maksym <shvets2616@gmail.com>
2025-01-04 19:42:56 +0100

    Develop dynamic screener

--- scoring/dynamic_scoring.py
+++ scoring/dynamic_scoring.py
@@ -1,50 +1,81 @@
+import datetime
 import os
+import csv
 import pandas as pd
 
 from constants import METRICS_DIR
 
 METRICS_WEIGHTS = {
+    "Total Debt": 0,
+    "Total Cash": 0,
+    "CFO": 0,
+    "Revenue": 0,
+    "Marcap": 0,
+
+    "EBITDA": 5,
     "Operating Margin": 5,
-    "Total Debt": 3,
-    "EBITDA": 8,
-    "CFO": 5,
-    "EV": 2,
-    "Stock Price": 0,
 
     # derivatives
+    "EV": 0,  # EV = Marcap + Total Debt - Total Cash
+
     "Total Debt / EBITDA": 7,
     "Total Debt / CFO": 7,
-    "Price / Sales": 10,
-    "Price / CFO": 9,
-    "Price / EV": 8,
+    "Marcap / Sales": 10,  # Marcap / Revenue
+    "Marcap / CFO": 9, # Marcap / CFO
+    "EV / EBITDA": 8,
 }
 
 # ----------
 # Directories
 WORKDIR = os.path.join(".")
 DYNAMIC_METRICS_DIR = os.path.join(METRICS_DIR, "dynamic_metrics")
+DATA_SOURCES = ["revenue.csv", "marcap.csv", "operating-margin.csv", "total-debt.csv", "ebitda.csv", "cfo.csv", "total-cash.csv"]
 
+def read_csv_to_df(filename: str) -> pd.DataFrame:
+    with open(os.path.join(DYNAMIC_METRICS_DIR, filename)) as f:
+        dialect = csv.Sniffer().sniff(f.readline(), delimiters=",;")
+
+    df = pd.read_csv(os.path.join(DYNAMIC_METRICS_DIR, filename), index_col=0, sep=dialect.delimiter, on_bad_lines='warn')
+    return df
 
 def prepare_time_series(df: pd.DataFrame):
     df.index = pd.to_datetime(df.index)
-    df = df[df.index.year >= 2014]
+    df = df[df.index.year >= 2015]
+
+    # Fill date gaps
+    full_date_range = pd.date_range(start=df.index.min(), end=datetime.date.today())
+    df = df.reindex(full_date_range)
+    df = df.ffill()
+
     df = df.map(lambda x: str(x).replace(",", ".")).astype(float)
     df.fillna(method="ffill", inplace=True)
     df.fillna(0, inplace=True)
     return df
 
+data = {
+    source.rstrip(".csv"): prepare_time_series(read_csv_to_df(source))
+    for source in DATA_SOURCES
+}
 
-_margin = pd.read_csv(os.path.join(DYNAMIC_METRICS_DIR, "operating-margin.csv"), index_col=0, sep=";", on_bad_lines='warn')
-margin = prepare_time_series(_margin)
+data["ev"] = data["marcap"] + data["total-debt"] - data["total-cash"]
+data["debt_to_ebitda"] = data["total-debt"] / data["ebitda"]
+data["debt_to_cfo"] = data["total-debt"] / data["cfo"]
+data["marcap_to_sales"] = data["marcap"] / data["revenue"]
+data["marcap_to_cfo"] = data["marcap"] / data["cfo"]
 
-_debt = pd.read_csv(os.path.join(DYNAMIC_METRICS_DIR, "total-debt.csv"), index_col=0, sep=";", on_bad_lines='warn')
-debt = prepare_time_series(_debt)
+# Debt metrics:
+last_1Y_chg = data["debt_to_cfo"]["$MRK"][-400:][np.where(data["debt_to_cfo"]["$MRK"][-400:].diff() != 0)[0]]
+q_gt05 = ((last_1Y_chg - last_1Y_chg.shift()) / last_1Y_chg > 0.05).mean()  # quarters with debt/cfo growth > 5%
 
-_ebitda = pd.read_csv(os.path.join(DYNAMIC_METRICS_DIR, "ebitda.csv"), index_col=0, sep=";", on_bad_lines='warn')
-ebitda = prepare_time_series(_ebitda)
 
-_cfo = pd.read_csv(os.path.join(DYNAMIC_METRICS_DIR, "cfo.csv"), index_col=0, sep=";", on_bad_lines='warn')
-cfo = prepare_time_series(_cfo)
+# Valuation metrics:
+q80 = data["marcap_to_sales"]["$MRK"].rolling(3*365).quantile(.8)[-1]
+q20 = data["marcap_to_sales"]["$MRK"].rolling(3*365).quantile(.2)[-1]
+last_val = data["marcap_to_sales"]["$MRK"][-1]
+norm_val = (last_val - q20) / (q80 - q20) # normalized by 3Y trailing quantile band
 
-debt_ebitda = debt / ebitda
+# Plots
+# import matplotlib.pyplot as plt
+# data["marcap_to_sales"].plot.line()
+# plt.savefig("marcap_to_sales.png")
 
--- screener/screen-stocks.py
+++ screener/screen-stocks.py
@@ -13,17 +13,22 @@ data = load_and_normalize_percentages(os.path.join(SCREENER_DIR, "screener-stock
 # Marcap
 data = data[data["Market Cap"] > 5e9]
 
-# Revenue growth
-data = data[(data["Rev. Growth 5Y"] > 0.03) | (pd.isna(data["Rev. Growth 5Y"]))]
-data = data[(data["Rev. Growth 3Y"] > 0.03) | (pd.isna(data["Rev. Growth 3Y"]))]
-data = data[(data["Rev. Growth"] > 0.03) | (pd.isna(data["Rev. Growth"]))]
+# Growth
+data = data[(data["Rev. Growth 5Y"] > 0.02) | (pd.isna(data["Rev. Growth 5Y"]))]
+data = data[(data["Rev. Growth 3Y"] > 0.02) | (pd.isna(data["Rev. Growth 3Y"]))]
+data = data[(data["Rev. Growth"] > 0.02) | (pd.isna(data["Rev. Growth"]))]
+data = data[(data["Rev. Growth (Q)"] > 0) | (pd.isna(data["Rev. Growth (Q)"]))]
 
-data = data[(data["Rev Gr. Next 5Y"] > 0.03) | (pd.isna(data["Rev. Growth 5Y"]))]
-data = data[(data["Rev Gr. Next Y"] > 0.03) | (pd.isna(data["Rev. Growth"]))]
+# data = data[(data["Rev Gr. Next 5Y"] > 0.02) | (pd.isna(data["Rev. Growth 5Y"]))]  # controversial because of long horizon.
+data = data[(data["Rev Gr. Next Y"] > 0.02) | (pd.isna(data["Rev. Growth"]))]
+data = data[(data["Rev Gr. Next Q"] > -0.02) | (pd.isna(data["Rev. Growth"]))]
 
-data = data[(data["OpInc Growth 5Y"] > 0.03) | (pd.isna(data["OpInc Growth 5Y"]))]
-data = data[(data["OpInc Growth 3Y"] > 0.03) | (pd.isna(data["OpInc Growth 3Y"]))]
-# data = data[data["OpInc Growth"] > 0.03]
+data = data[(data["OpInc Growth 5Y"] > 0.02) | (pd.isna(data["OpInc Growth 5Y"]))]
+data = data[(data["OpInc Growth 3Y"] > 0.02) | (pd.isna(data["OpInc Growth 3Y"]))]
+# data = data[data["OpInc Growth"] > 0.03 | (pd.isna(data["OpInc Growth 3Y"]))]
+# data = data[data["OpInc Growth (Q)"] > 0 | (pd.isna(data["OpInc Growth (Q)"]))]
+
+# Note: Lacking OCF.
 
 # Z-score
 data = data[(data["Z-Score"] > 3) | (pd.isna(data["Z-Score"]))]
@@ -31,8 +36,13 @@ data = data[(data["Z-Score"] > 3) | (pd.isna(data["Z-Score"]))]
 # ROA / ROI
 data = data[(data["ROA"] > 0.03) | (pd.isna(data["ROA"]))]
 data = data[(data["ROA (5Y)"] > 0.03) | (pd.isna(data["ROA (5Y)"]))]
-data = data[(data["ROIC"] > 0.03) | (pd.isna(data["ROIC"]))]
-data = data[(data["ROIC (5Y)"] > 0.03) | (pd.isna(data["ROIC (5Y)"]))]
+data = data[(data["ROIC"] > 0.05) | (pd.isna(data["ROIC"]))]
+data = data[(data["ROIC (5Y)"] > 0.05) | (pd.isna(data["ROIC (5Y)"]))]
 
+data = data[(data["Oper. Margin"] > 0.08) | (pd.isna(data["Oper. Margin"]))]
+
+# Analysts / Upside
+data = data[(data["Analyst Count"] > 5) | (pd.isna(data["Analyst Count"]))]
+data = data[(data["Price Target Upside (%)"] > 0.05) | (pd.isna(data["Price Target Upside (%)"]))]
 
 print("Done.")
