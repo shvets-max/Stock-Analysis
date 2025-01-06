0275fddf850194fd425858badf8fd39e4fe61e6f
Maksym <shvets2616@gmail.com>
2025-01-06 12:45:36 +0100

    exploring dynamic scoring implementation

--- scoring/dynamic_scoring.py
+++ scoring/dynamic_scoring.py
@@ -66,24 +66,23 @@ data["marcap_to_sales"] = data["marcap"] / data["revenue"]
 data["marcap_to_cfo"] = data["marcap"] / data["cfo"]
 data["ev_to_ebitda"] = data["ev"] / data["ebitda"]
 
-# EBITDA and Operating Margin
-# Objective: not decreasing (avg. chg.), stable growth (std)
-def calculate_avg_std(series: pd.DataFrame, timestamp: int = 365):
-    unique_vals = series[-timestamp:][np.where(series[-timestamp:].diff() != 0)[0]]
+def calculate_avg_std(series: pd.DataFrame, timerange: int = 365):
+    """
+    Calculate average growth rate and how stable it is.
+    :param series:
+    :param timerange:
+    :return:
+    """
+    unique_vals = series[-timerange:][np.where(series[-timerange:].diff() != 0)[0]]
     perc_chg = (unique_vals - unique_vals.shift()) / unique_vals
     avg_chg = perc_chg[-6:].mean()
     std_to_mean = perc_chg.std() / avg_chg
 
-    return avg_chg, std_to_mean
+    return avg_chg, abs(std_to_mean)
 
-ebitda_chg, ebitda_std = calculate_avg_std(data["ebitda"]["$MRK"], 3*365)
-op_margin_chg, op_margin_std = calculate_avg_std(data["operating-margin"]["$MRK"], 3*365)
-
-# Debt metrics
-# (desirable: not a high debt or not increasing rapidly and regularly decreased)
 def normalize_ewm_growth(series: pd.Series, timerange: int = 365, low: float = .0, high: float = .05) -> pd.Series:
     """
-
+    Calculate exponential moving average and normalize it by a given value range.
     :param timerange:
     :param series:
     :param low:
@@ -97,44 +96,29 @@ def normalize_ewm_growth(series: pd.Series, timerange: int = 365, low: float = .
     norm_ewm = (ewm - low) / (high - low)
     return norm_ewm
 
-def calculate_debt_dynamic_score(series: pd.Series, threshold: float) -> float:
+def normalize_growth_rate(series: pd.Series, timerange: int = 365, low: float = .0, high: float = .05) -> pd.Series:
     """
-    Measure debt dynamic. 0 - good, 1 - bad.
-    :param threshold:
+    Calculate exponential moving average and normalize it by a given value range.
+    :param timerange:
     :param series:
+    :param low:
+    :param high:
     :return:
     """
-    # 1. If above threshold
-    above_thresh = series[-1] > threshold
 
-    # 2. Is last 5 quarters debt growth is "under control" (for how many quarters 3Y ewm < threshold)
-    series_norm_ewm = normalize_ewm_growth(series, 3*365, low=-0.02, high=0.05)
-    q5 = np.array([series_norm_ewm[-q:].mean() for q in range(5, 0, -1)])
-    debt_under_control = (q5 <= 1).mean()
+    unique_vals = series[-timerange:][np.where(series[-timerange:].diff() != 0)[0]]
+    perc_chg = (unique_vals - unique_vals.shift()) / unique_vals
+    norm_chg = (perc_chg - low) / (high - low)
+    return norm_chg
 
-    # 3. Non-increasing debt score. 0 - increasing all the time, 1 - non-increasing all the time.
-    epsilon = 0.1
-    decrease_points = 0
-    for x in range(4):
-        decrease_points += (q5[x] + epsilon >= q5[x+1:]).sum()
-    decrease_ratio = decrease_points / 10
-
-    # 1st component tells if debt is high,
-    # 2nd and 3rd components tell whether company keeps it under control and reduces it regularly.
-    return float(above_thresh) * (1 - (debt_under_control + decrease_ratio) / 2)
-
-debt_to_cfo_score = calculate_debt_dynamic_score(data["debt_to_cfo"]["$MRK"], threshold=3)
-debt_to_ebitda_score = calculate_debt_dynamic_score(data["debt_to_ebitda"]["$MRK"], threshold=3)
-
-# Valuation metrics:
-def normalize_by_rolling_quantile_band(
+def normalize_by_quantile_band(
         series: pd.Series,
         timerange: int = 365,
         q_low: float = .2,
         q_high: float = .8
 ) -> float:
     """
-    Normalize last datapoint to quantile band along given timerange.
+    Normalize last datapoint to quantile band along the given timerange.
 
     :param series:
     :param timerange:
@@ -146,9 +130,54 @@ def normalize_by_rolling_quantile_band(
     ql = series[-timerange:].quantile(q_low)
     return (series[-1] - ql) / (qh - ql)
 
-ps_score = normalize_by_rolling_quantile_band(data["marcap_to_sales"]["$MRK"], 3*365)
-p_cfo_score = normalize_by_rolling_quantile_band(data["marcap_to_cfo"]["$MRK"], 3*365)
-ev_ebitda_score = normalize_by_rolling_quantile_band(data["ev_to_ebitda"]["$MRK"], 3*365)
+def calculate_debt_dynamic_score(series: pd.Series, threshold: float, smoothing: bool = True, n_quarters: int = 5) -> float:
+    """
+    Measure debt dynamic. 0 - good, 1 - bad.
+    :param n_quarters:
+    :param smoothing:
+    :param threshold:
+    :param series:
+    :return:
+    """
+    # 1. If above threshold
+    above_thresh = series[-1] > threshold
+
+    # 2. Is last 5 quarters debt growth is "under control" (for how many quarters 3Y ewm < threshold)
+    if smoothing:
+        series_norm = normalize_ewm_growth(series, 3*365, low=-0.02, high=0.05)
+    else:
+        series_norm = normalize_growth_rate(series, 3*365, low=-0.02, high=0.05)
+
+    qs = np.array([series_norm[-q:].mean() for q in range(n_quarters, 0, -1)])
+    duc_quarters = (qs <= 1).mean()  # debt-under-control quarters
+
+    # 3. Non-increasing debt score. 0 - increasing all the time, 1 - non-increasing all the time.
+    epsilon = 0.1
+    debt_down_points = 0
+    for x in range(n_quarters - 1):
+        debt_down_points += (qs[x] + epsilon >= qs[x+1:]).sum()
+    debt_down_ratio = debt_down_points / 10
+
+    # 1st component tells if debt is high,
+    # 2nd and 3rd components tell whether company keeps it under control and reduces it regularly.
+    return float(above_thresh) * (1 - (duc_quarters + debt_down_ratio) / 2)
+
+# EBITDA and Operating Margin
+# Objective: to reward companies, for the metric is not decreasing (avg. chg.) and growing stably(std).
+ebitda_chg, ebitda_std = calculate_avg_std(data["ebitda"]["$MRK"], 3*365)
+op_margin_chg, op_margin_std = calculate_avg_std(data["operating-margin"]["$MRK"], 3*365)
+
+# Debt metrics
+# Objective: to punish only companies with high debt,
+# but reward if they don't increase it rapidly and/or regularly reduce it.
+debt_to_cfo_score = calculate_debt_dynamic_score(data["debt_to_cfo"]["$MRK"], threshold=3)
+debt_to_ebitda_score = calculate_debt_dynamic_score(data["debt_to_ebitda"]["$MRK"], threshold=3)
+
+# Valuation metrics:
+# Objective: reward underpriced, punish overpriced.
+ps_score = normalize_by_quantile_band(data["marcap_to_sales"]["$MRK"], 3*365, q_low=0.2, q_high=0.8)
+p_cfo_score = normalize_by_quantile_band(data["marcap_to_cfo"]["$MRK"], 3*365, q_low=0.2, q_high=0.8)
+ev_ebitda_score = normalize_by_quantile_band(data["ev_to_ebitda"]["$MRK"], 3*365, q_low=0.2, q_high=0.8)
 
 # Plots
 # import matplotlib.pyplot as plt
